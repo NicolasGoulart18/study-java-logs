80605abfced442daef4d0b2710cd2cc48e675ce9
NicolasGoulart18 <nicolasgoulartt2008@gmail.com>
2026-08-10 18:20:17 -0300

    Consegui entender e aprender como fazer com for-each e utilizando a navegação comum com for i

--- Study-java-logs/Java/ArraysMultidimensionais/ArrayMultidimensionais02.java
+++ Study-java-logs/Java/ArraysMultidimensionais/ArrayMultidimensionais02.java
@@ -2,26 +2,36 @@ package ArraysMultidimensionais;
 
 public class ArrayMultidimensionais02 {
     public static void main(String[] args) {
-        int [] [] arrayInt=new int[3][3];
+        int [] [] arrayInt=new int[3][4];
 
+        //Atribuição de todos arrays
         arrayInt [0][0]=1;
         arrayInt [0][1]=2;
         arrayInt [0][2]=3;
+        arrayInt [0][3]=4;
 
-        arrayInt [1][0]=4;
-        arrayInt [1][1]=5;
-        arrayInt [1][2]=6;
+        arrayInt [1][0]=5;
+        arrayInt [1][1]=6;
+        arrayInt [1][2]=7;
+        arrayInt [1][3]=8;
 
-        arrayInt [2][0]=7;
-        arrayInt [2][1]=8;
-        arrayInt [2][2]=9;
+        arrayInt [2][0]=9;
+        arrayInt [2][1]=10;
+        arrayInt [2][2]=11;
+        arrayInt [2][3]=12;
+
+        //Se eu quiser fazer com for I faço assim:
+        for (int i = 0; i <arrayInt.length ; i++) {
+            for (int j = 0; j < arrayInt[i].length ; j++) {
+                System.out.println(arrayInt[i][j]);
+            }
+        }
 
 
-
-
-        //Dessa forma da erro pois a variavel de referencia qnd nao tem nada
-        // consta como null
-        //Array e sempre uma variable de referencia ou seja um objeto
+                                                                     //Dessa forma da erro pois a variavel de referencia qnd nao tem nada
+                                                                    //consta como null
+                                                                    //Array e sempre uma variable de referencia ou seja um objeto
+       //Se eu quiser fazer usando for-each
         for(int[]arrBase:arrayInt){
             for (int num:arrBase)
                 System.out.println(num);
