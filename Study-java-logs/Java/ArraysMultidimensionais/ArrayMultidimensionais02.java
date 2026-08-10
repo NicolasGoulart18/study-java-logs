9cb980a8396ce98ec72843a8ee8ade45004f68cf
NicolasGoulart18 <nicolasgoulartt2008@gmail.com>
2026-08-10 18:56:01 -0300

    Outra forma que aprendi agr, é antes eu declarar uma variavel array colocar valores{}, e dps so passar essa variavel para meu array com referencia

--- Study-java-logs/Java/ArraysMultidimensionais/ArrayMultidimensionais02.java
+++ Study-java-logs/Java/ArraysMultidimensionais/ArrayMultidimensionais02.java
@@ -3,17 +3,25 @@ package ArraysMultidimensionais;
 public class ArrayMultidimensionais02 {
     public static void main(String[] args) {
         int [] [] arrayInt=new int[3][];
-
+        int[] array={2,3,4};
 
         //Forma de inicialzação quando o array multidimensional o "j" é null
         //Declaro um objeto para ele
+        /*Posso declara dessa forma tbm:
+          int[] array={1,2,3};
+          int [][] arrayInt= new int[3][];
+          arrayInt[1]=array
+
+         */
         arrayInt[0] = new int[1];
         arrayInt[0][0]=1;
 
-        arrayInt[1]= new int[3];
-        arrayInt[1][0]=2;
+        arrayInt[1]=array;
+        /*arrayInt[1][0]=2;
         arrayInt[1][1]=3;
         arrayInt[1][2]=4;
+
+         */
 
         arrayInt[2]=new int[6];
         arrayInt[2][0]=5;
