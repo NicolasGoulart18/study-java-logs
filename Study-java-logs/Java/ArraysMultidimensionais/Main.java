4c0f4446a9f741ddaa9d87c1713f0c1c74b37f83
NicolasGoulart18 <nicolasgoulartt2008@gmail.com>
2026-08-09 21:54:31 -0300

    Na verdade no 2 for ele deveria ser navegado pelo indici i do primeiro for e nao pelo numero do indice (0)

--- Study-java-logs/Java/ArraysMultidimensionais/Main.java
+++ Study-java-logs/Java/ArraysMultidimensionais/Main.java
@@ -16,7 +16,7 @@ public class Main {
 
         for (int i = 0; i < dias.length ; i++) {
         //Nesse primeiro for ele navega pelo primeiro array, que seria a posição 0
-            for (int j = 0; j <dias[0].length ; j++) {
+            for (int j = 0; j <dias[i].length ; j++) {
                 //ja nesse array ele navega dentro da posicao 0 que faz referencia ao 2 array
                 System.out.println(dias[i][j]);
             }
