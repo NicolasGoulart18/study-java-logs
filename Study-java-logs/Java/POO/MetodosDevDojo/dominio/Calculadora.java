6b0e4f85a3ffbb02e0747b6d56f6a36076b8f165
NicolasGoulart18 <nicolasgoulartt2008@gmail.com>
2026-08-11 16:02:45 -0300

    Formas diferentes de fazer return nos metodos

--- Study-java-logs/Java/POO/MetodosDevDojo/dominio/Calculadora.java
+++ Study-java-logs/Java/POO/MetodosDevDojo/dominio/Calculadora.java
@@ -15,7 +15,26 @@ public class Calculadora {
     }
 
     public double divideDoisNumeros(double num1,double num2) {
-        return num1/num2;
+        if(num2==0){
+            return 0;
+        }
+        return (num1/num2);
     };
+
+    public double divideDoisNumeros2(double num1,double num2){
+        if(num2 !=0){
+            return (num1/num2);
+        }
+        return 0;
+    }
+
+    public void imprimeDivisaoDeDoisNumeros(double num1, double num2){
+        if(num2==0){
+            System.out.println("Nao existe divisão por zero");
+            return;
+        }else{
+            System.out.println(num1/num2);
+        }
+    }
 
 }
