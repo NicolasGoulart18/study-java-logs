b31214df2a7c6de35dc84a58bf9b41d337029c64
NicolasGoulart18 <nicolasgoulartt2008@gmail.com>
2026-08-11 11:38:05 -0300

    fix

--- Study-java-logs/Java/POO/Classes/test/Main.java
+++ Study-java-logs/Java/POO/Classes/test/Main.java
@@ -4,20 +4,22 @@ import POO.Classes.dominio.Carro;
 
 public class Main {
     static void main(String[] args) {
+
         Carro carro1 = new Carro();
         Carro carro2 = new Carro();
+
         carro1.nome="Chevrolet Onix";
         carro1.modelo="Onix";
         carro1.ano=2019;
 
-        System.out.println("Carro: "+carro1.nome+" Modelo: "+carro1.modelo+" Ano: "+carro1.ano);
-        System.out.println();
-        System.out.println("-----------------------------------------");
-        System.out.println();
-
         carro2.nome="Ford Mustang";
         carro2.modelo="Mustang";
         carro2.ano=1967;
+
+        System.out.println("Carro: "+carro1.nome+" Modelo: "+carro1.modelo+" Ano: "+carro1.ano);
+        System.out.println();
+        System.out.println("================================================");
+        System.out.println();
         System.out.println("Carro: "+carro2.nome+" Modelo: "+carro2.modelo+" Ano: "+carro2.ano);
     }
 }
